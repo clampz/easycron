f3ee24ffc40a3b69ccfa5bc3675e5d1ebd8dbd79
david <david@weinman.com>
2013-05-23 02:05:18 -0700

    installer fixed
    updated readme a little too

--- README.md
+++ README.md
@@ -3,6 +3,11 @@ easycron
 
 This is a utility for modifying a user's crontab
 
+Easycron's  main  usage  is  to  add processes to the user's crontab without having to know the format that crontab accepts.
+
+For example, ```easycron 30min './example.sh'``` adds the ```./example.sh``` process to the crontab to run every 30 min.
+
+
 to install, run: 
 ```
 sudo ./install.sh
--- install.sh
+++ install.sh
@@ -1,14 +1,41 @@
 #!/bin/bash
 
-mkdir ~/bin
+if [ -d ~/bin ]; then
+  : 
+else
+  mkdir ~/bin
+fi
+
+#mkdir ~/bin
 
 mv easycron.sh ~/bin/
 
-echo 'if [ -d $HOME/bin ]; then
+#if [[ `uname` == 'Linux' ]]; then
+#  cat ~/.bashrc > my_bash_profile
+#fi
+#OS X below
+#cat ~/.bash_profile > my_bash_profile
+
+if ! grep -q 'PATH=$PATH:$HOME/bin' "$HOME/.bash_profile"; then
+  echo 'if [ -d $HOME/bin ]; then
         PATH=$PATH:$HOME/bin
+        fi' >> ~/.bash_profile
+fi
+if [[ `uname` == 'Linux' ]]; then
+  if ! grep -q 'alias easycron' "$HOME/.bashrc"; then
+    echo 'alias doesnt exist in bashrc'
+    echo "alias easycron='easycron.sh'" >> ~/.bashrc
+  fi
+elif [[ `uname` == 'Darwin' ]]; then
+  if ! grep -q 'alias easycron' "$HOME/.bash_profile"; then
+    echo "alias easycron='easycron.sh'" >> ~/.bash_profile
+  fi
 fi
 
-alias easycron='easycron.sh'' >> ~/.bash_profile
+#OS X below
+#cp my_bash_profile ~/.bash_profile
+
+#cat ~/.bash_profile
 
 mv easycron.7 /usr/share/man/man7/
 
